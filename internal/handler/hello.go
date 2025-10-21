b5d56de8d2a577e899529fc87494cd865f3c159e
cirrostack <dlaviniuraul@gmail.com>
2025-10-21 23:44:51 +0300

    test-ssh

--- internal/handler/hello.go
+++ internal/handler/hello.go
@@ -9,3 +9,5 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(map[string]string{"message": "Salut!"})
 }
+
+
